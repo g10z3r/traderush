eb2f6263c6af707fb9dc30f88a6b90478b2a0d82
g10z3r <20g10z3r@gmail.com>
2026-06-10 11:37:31 +0200

    project init

---
+++ src/client/java/traderush/client/TradeRushClient.java
@@ -0,0 +1,10 @@
+package traderush.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TradeRushClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
